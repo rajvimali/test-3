6fe94c8858f0efcd4e7e0be2f78b034232f9981e
Rajvi <rajvimali1112@gmail.com>
2023-11-23 12:02:25 +0530

    Test-3 Completed

---
+++ src/Components/Action.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import './Style.css'
+// const dispatch =useDispatch()
+
+function Action() {
+    const state = useSelector((state) => state.product)
+    console.log(state)
+
+    return (
+        <>
+            <div className='row '>
+
+                {state && state.map((item, index) =>
+                    <div className="product-card col-3" key={item.id}>
+                        <div class="product-tumb">
+                            <img src={item.img} alt="" className="img-fliud" />
+                        </div>
+                        <div class="product-details">
+                            <span class="product-catagory">{item.type}</span>
+                            <h4><a href="">{item.name}</a></h4>
+                            <p>{item.color}</p>
+                            <a className='btn'>
+                                Add Cart +
+                            </a>
+                        </div>
+                    </div>
+                )}
+
+            </div>
+        </>
+    )
+}
+export default Action
---
+++ src/Components/Reducer.jsx
@@ -0,0 +1,25 @@
+const initial = {
+    product: [
+        {
+            name: 'Shirt', color: 'white', type: 'men', material: 'polyster', brand: 'nike', img: 'https://www.aristobrat.in/cdn/shop/products/CS_ClassicWhite_06_1080x.jpg?v=1614938700&#39;'
+        },
+        { name: 'Shirt', color: 'red', type: 'women', material: 'cotton', brand: 'adidas', img: 'https://rukminim1.flixcart.com/image/850/1000/ktx9si80/top/a/q/k/m-sr-31-m-red-wakshi-original-imag763gejeh2gd6.jpeg?q=20&#39;' },
+        { name: 'Shirt', color: 'navy', type: 'kids', material: 'polyster', brand: 'nike', img: 'https://toyszoom.net/cdn/shop/products/Korean-Long-Sleeve-Teen-Boys-Shirts-Yellow-Red-Green-Plaid-Top-For-Children-Kids-Teenage-Clothes_large.jpg?v=1537697364;' },
+        { name: 'Shirt', color: 'navy', type: 'men', material: 'polyster', brand: 'nike', img: 'https://thehouseofrare.com/cdn/shop/products/IMG_0187_34668d36-3101-432c-9aeb-cd3caa784b8a_765x.jpg?v=1674802116;' },
+        { name: 'T-shirt', color: 'white', type: 'kids', material: 'cotton', brand: 'us polo', img: 'https://thewpopshop.com.au/cdn/shop/products/Baa_Baa_Blue_Sheep_Nursery_Rhyme_T-Shirt_Boy_1024x.jpg?v=1668876640;' },
+        { name: 'Shirt', color: 'white', type: 'women', material: 'cotton', brand: 'us polo', img: 'https://www.dhresource.com/0x0/f2/albu/g21/M01/62/F0/rBNaOWCI5CmAFa7YAASJeVSE_sI173.jpg;' },
+        { name: 'T-shirt', color: 'red', type: 'kids', material: 'cotton', brand: 'nike', img: 'https://cdn.shopify.com/s/files/1/1460/3818/products/4243c41e-05c5-57be-b4ee-541411978fa4_620x.jpg?v=1672840241;' },
+        { name: 'Shirt', color: 'navy', type: 'women', material: 'cotton', brand: 'us polo', img: 'https://assets.ajio.com/medias/sys_master/root/20230519/7WSW/646794b442f9e729d79d2f53/-473Wx593H-443014940-navy-MODEL.jpg;' },
+        { name: 'T-shirt', color: 'red', type: 'men', material: 'polyster', brand: 'adidas', img: 'https://www.aelomart.com/cdn/shop/products/Round-Neck-Casual-Men_s-T-Shirt-in-Red_1024x1024.jpg?v=1559850342;' },
+        { name: 'Shirt', color: 'green', type: 'men', material: 'polyster', brand: 'us polo', img: 'https://img0.junaroad.com/uiproducts/19717972/zoom_0-1681297412.jpg;' },
+        { name: 'T-shirt', color: 'green', type: 'kids', material: 'polyster', brand: 'us polo', img: 'https://southhorizon.com/cdn/shop/products/964-7-01.jpg?v=1672263399;' },
+        { name: 'Shirt', color: 'green', type: 'women', material: 'polyster', brand: 'us polo', img: 'https://assets0.mirraw.com/images/8726966/SFSHRT2008-1_large.jpg?1613454801;' }
+    ]
+}
+
+
+const Reducer = (state = initial, action) => {
+
+    return state
+}
+export default Reducer
